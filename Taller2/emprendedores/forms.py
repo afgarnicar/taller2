5ec0cba70d519a1004e4f94357b6f3cc44dc62a6
Andres Felipe Garnica Roa <140566421+afgarnicar@users.noreply.github.com>
2025-03-03 16:53:19 -0500

    Delete Taller2 directory

--- Taller2/emprendedores/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Entrepreneur
-
-class EntrepreneurForm(forms.ModelForm):
-    class Meta:
-        model = Entrepreneur
-        fields = ['business_name', 'location', 'schedule', 'contact_info']
