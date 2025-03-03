70c75cbf886a3cde28e4317a3ffcb572d146e6ab
Andres Felipe Garnica Roa <140566421+afgarnicar@users.noreply.github.com>
2025-03-03 09:33:41 -0500

    Delete Taller2 directory

--- Taller2/emprendedores/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Entrepreneur
-
-admin.site.register(Entrepreneur)
--- Taller2/emprendedores/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import entrepreneur_list, create_entrepreneur
-
-urlpatterns = [
-    path('emprendedores/', entrepreneur_list, name='entrepreneur_list'),
-    path('crear-emprendedor/', create_entrepreneur, name='create_entrepreneur'),
-]
